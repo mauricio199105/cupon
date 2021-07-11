1356a332d0e6c00525cd2b769662f97f544f6c2d
mauricio199105 <mejiam756@gmail.com>
2021-07-11 11:21:17 -0500

    incluyo un index

--- src/main/java/com/ml/cupon/controller/CouponController.java
+++ src/main/java/com/ml/cupon/controller/CouponController.java
@@ -3,6 +3,7 @@ package com.ml.cupon.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,6 +33,12 @@ public class CouponController {
 					.body("Items not found!.");
 		}
 
+	}
+	
+
+	@GetMapping(value = "index", produces = "application/json")
+	public ResponseEntity<Object> getStats() {
+		return ResponseEntity.status(HttpStatus.OK).contentType(MediaType.APPLICATION_JSON).body("Hola");
 	}
 
 }
